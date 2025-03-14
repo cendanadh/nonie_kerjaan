3c1af1e810b3340451b1f22833994dfb17b6ea90
Dharmawan Cendana <76508470+cendanadh@users.noreply.github.com>
2025-03-14 12:32:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def index():
     return render_template('index.html', table_html=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
